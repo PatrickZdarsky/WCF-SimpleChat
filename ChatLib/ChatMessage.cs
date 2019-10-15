dddc323aa5d5bd348ee681c6234ca47beebe68cf
Rxcki <rxckiat@gmail.com>
2019-10-15 09:57:13 +0200

    Add Lib

---
+++ ChatLib/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatLib
+{
+    class ChatMessage
+    {
+        [DataMember]
+        public DateTime dateTime { get; set; }
+        [DataMember]
+        public string userName { get; set; }
+        [DataMember]
+        public string message { get; set; }
+    }
+}
---
+++ ChatLib/IChat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatLib
+{
+    [ServiceContract]
+    interface IChat
+    {
+    }
+}
